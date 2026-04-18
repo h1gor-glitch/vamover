0c6a88ed4c533d498b78ef3b5d67a886769df8fd
h1gor-glitch <jose.moises@academico.ifpb.edu.br>
2026-04-18 10:12:21 -0300

    valo la

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("criarConta").addEventListener("click", function(e) {
+    e.preventDefault(); // evita o comportamento padrão do link
+    window.location.href = "cadastro.html"; // vai para outra página
+});
